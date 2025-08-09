70c5998a01ca607e487f99da2efb92689f1cc663
ramprin <ramyapriyadarshini78@gmail.com>
2025-08-10 01:55:05 +0530

    Initial Commit

---
+++ src/main/java/com/example/spring_jpa_notes/SpringJpaNotesApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_jpa_notes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaNotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_jpa_notes/SpringJpaNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_jpa_notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
